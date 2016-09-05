ca96af7248d82d8bc753b33891688d49509582fb
Kevin Eddye <kevneddy@gmail.com>
2016-09-05 13:42:42 -0700

    Add db for posts and sanitized input

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,30 +2,45 @@
 # Database access functions for the web forum.
 # 
 
-import time
+import psycopg2
+import bleach
 
-## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    '''Get all the posts from the database, sorted with the newest first.'''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
 
-    Returns:
+    '''Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    c.ececute("SELECT time, content FROM posts ORDER BY time DESC")
+    posts = ({'content': str(row[1]), 'time':str(row[0])})
+            for row in c.fetchall())
+    DB.close()
     return posts
 
 ## Add a post to the database.
 def AddPost(content):
-    '''Add a new post to the database.
+    '''Add a new post to the database.'''
+    DB = psycopg2.connect("dbname=forum")
+    c = DB.cursor()
+    c.ececute("INSERT INTO posts (content) VALUES (%s)", (content,))
+    DB.commit()
+    DB.close()
 
-    Args:
+    '''Args:
       content: The text content of the new post.
     '''
     t = time.strftime('%c', time.localtime())
     DB.append((t, content))
+
+
+
+
+
+
+
